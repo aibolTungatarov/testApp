4095fdf92fe5ae671e920031cd8ca0603ae2e8e8
Айбол <ATunggatarov@beeline.kz>
2023-09-28 18:40:21 +0600

    Initial Commit

---
+++ TestProj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestProj
+//
+//  Created by Айбол on 28.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
